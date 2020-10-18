f0985a66f5836a03ff729d81012807136c88e128
MuTsunTsai <don.m.t.tsai@gmail.com>
2020-10-18 19:09:18 +0800

    Template precompile

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 
-import ts, { ClassElement, SyntaxKind } from "typescript";
+import ts, { ClassElement, SyntaxKind } from 'typescript';
+import { compile } from 'vue-template-compiler';
 
 function getFunction(m: ts.FunctionLikeDeclarationBase, s: ts.SourceFile, name?: string) {
-	if(!name) name = m.name.getText(s);
-	let body = m.body.getText(s);
+	if(!name) name = m.name?.getText(s);
+	let body = m.body?.getText(s);
 	let param = m.parameters.map(p => p.name.getText(s)).join(",");
 	return `${name}(${param})${body}`;
 }
@@ -20,43 +21,36 @@ function getDecoratorArguments(m: ClassElement, decorator: string, s: ts.SourceF
 	return undefined;
 }
 
-function getComponentOption(dec: ts.ClassDeclaration): componentOption {
-	for(let d of dec.decorators) {
-		if(ts.isIdentifier(d.expression) &&
-			d.expression.escapedText == "Component") return {};
-		if(ts.isCallExpression(d.expression) &&
-			ts.isIdentifier(d.expression.expression) &&
-			d.expression.expression.escapedText == "Component" &&
-			d.expression.arguments.length) {
-			let op = d.expression.arguments[0];
-			if(ts.isObjectLiteralExpression(op)) {
-				let result: componentOption = {};
-				for(let p of op.properties) {
-					if(ts.isPropertyAssignment(p) &&
-						ts.isIdentifier(p.name) &&
-						ts.isStringLiteral(p.initializer)) {
-						if(p.name.escapedText == "name") result.name = p.initializer.text;
-						if(p.name.escapedText == "template") result.template = p.initializer.text;
+function getComponentOption(dec: ts.ClassDeclaration): componentOption | undefined {
+	if(dec.decorators)
+		for(let d of dec.decorators) {
+			if(ts.isIdentifier(d.expression) &&
+				d.expression.escapedText == "Component") return {};
+			if(ts.isCallExpression(d.expression) &&
+				ts.isIdentifier(d.expression.expression) &&
+				d.expression.expression.escapedText == "Component" &&
+				d.expression.arguments.length) {
+				let op = d.expression.arguments[0];
+				if(ts.isObjectLiteralExpression(op)) {
+					let result: componentOption = {};
+					for(let p of op.properties) {
+						if(ts.isPropertyAssignment(p) &&
+							ts.isIdentifier(p.name) &&
+							ts.isStringLiteral(p.initializer)) {
+							if(p.name.escapedText == "name") result.name = p.initializer.text;
+							if(p.name.escapedText == "template") result.template = p.initializer.text;
+						}
 					}
+					return result;
 				}
-				return result;
 			}
 		}
-	}
 	return undefined;
 }
 
 interface componentOption {
 	name?: string;
 	template?: string;
-}
-
-interface TranspileResult {
-	/** Resulting script. */
-	script: string;
-
-	/** The ID of the template. */
-	template: string;
 }
 
 /** Built-in hooks of Vue.js */
@@ -75,90 +69,117 @@ var LIFECYCLE_HOOKS = [
 	'serverPrefetch'
 ];
 
+function findClassDeclaration(statements: readonly ts.Statement[]) {
+	for(var s of statements) {
+		if(ts.isClassDeclaration(s) &&
+			s.modifiers?.some(m => m.kind == ts.SyntaxKind.ExportKeyword) &&
+			s.modifiers?.some(m => m.kind == ts.SyntaxKind.DefaultKeyword)) {
+
+			let comOption = getComponentOption(s);
+			if(comOption !== undefined) return {
+				statement: s,
+				option: comOption
+			}
+		}
+	}
+	throw new Error("Cannot find class declaration");
+}
+
+function classToComponentOptionString(statement: ts.ClassDeclaration, sourceFile: ts.SourceFile): string[] {
+	let props: string[] = [];
+	let data: string[] = [];
+	let watch: string[] = [];
+	let computed: string[] = [];
+	let methods: string[] = [];
+	let events: string[] = [];
+	let provides: string[] = [];
+	let injects: string[] = [];
+
+	for(let member of statement.members) {
+		if(!member.name) continue;
+
+		let name = member.name.getText(sourceFile);
+		if(ts.isPropertyDeclaration(member)) {
+			let ini = member.initializer;
+			let init = ini ? ini.getText(sourceFile) : "undefined";
+			let prop = getDecoratorArguments(member, "Prop", sourceFile);
+			let inject = getDecoratorArguments(member, "Inject", sourceFile);
+			if(prop) {
+				if(ini) props.push(`${name}:{type:${prop[0]},default:${init}}`);
+				else props.push(`${name}:${prop[0]}`);
+			} else if(inject !== undefined) {
+				if(!inject.length) inject[0] = `'${name}'`;
+				injects.push(`${name}: ${inject[0]}`);
+			} else {
+				data.push(`${name}:${init}`);
+				let provide = getDecoratorArguments(member, "Provide", sourceFile);
+				if(provide) provides.push(`${provide}: this.${name}`);
+			}
+		}
+		if(ts.isGetAccessor(member)) {
+			computed.push(getFunction(member, sourceFile));
+		}
+		if(ts.isMethodDeclaration(member)) {
+			let func = getFunction(member, sourceFile);
+			if(member.modifiers) for(let mod of member.modifiers) {
+				if(mod.kind == SyntaxKind.AsyncKeyword) {
+					func = "async " + func;
+					break;
+				}
+			}
+			let tag = getDecoratorArguments(member, "Watch", sourceFile);
+			if(tag) watch.push(getFunction(member, sourceFile, tag[0]));
+			else {
+				if(LIFECYCLE_HOOKS.includes(name)) events.push(func);
+				else methods.push(func);
+			}
+		}
+	}
+
+	let options: string[] = [];
+
+	if(data.length) options.push(`data() { return { ${data.join(",")} }; }`);
+	if(props.length) options.push(`props: { ${props.join(",")} }`);
+	if(provides.length) options.push(`provide() { return { ${provides.join(",")} }; }`);
+	if(injects.length) options.push(`inject: { ${injects.join(",")} }`);
+	if(watch.length) options.push(`watch: { ${watch.join(",")} }`);
+	if(computed.length) options.push(`computed: { ${computed.join(",")} }`);
+	if(methods.length) options.push(`methods: { ${methods.join(",")} }`);
+	options.push(...events);
+
+	return options;
+}
+
 /**
  * Transpile the default exported class to Vue component global registration.
  * @param code Original Typescript code.
  */
-function VPDtoJs(code: string): TranspileResult {
+function VPDtoJs(code: string, template: string): string {
 	var sourceFile = ts.createSourceFile("vue.ts", code, ts.ScriptTarget.ESNext, false);
 
-	for(var s of sourceFile.statements) {
-		if(ts.isClassDeclaration(s) &&
-			s.modifiers.some(m => m.kind == ts.SyntaxKind.ExportKeyword) &&
-			s.modifiers.some(m => m.kind == ts.SyntaxKind.DefaultKeyword)) {
+	var { statement, option } = findClassDeclaration(sourceFile.statements);
 
-			let comOption = getComponentOption(s);
+	let comName = "name" in option ? option.name : statement.name?.escapedText.toString().toLowerCase();
+	if(!comName) throw new Error("Component needs to have a name");
 
-			let comName = "name" in comOption ? comOption.name : s.name.escapedText.toString().toLowerCase();
-			let template = "template" in comOption ? comOption.template : `#${comName}`;
-			let match = template.match(/^#(.+)$/);
-			let templateName = match ? match[1] : comName;
-			let props: string[] = [];
-			let data: string[] = [];
-			let watch: string[] = [];
-			let computed: string[] = [];
-			let methods: string[] = [];
-			let events: string[] = [];
-			let provides: string[] = [];
-			let injects: string[] = [];
+	let options = classToComponentOptionString(statement, sourceFile);
 
-			for(let m of s.members) {
-				let name = m.name.getText(sourceFile);
-				if(ts.isPropertyDeclaration(m)) {
-					let ini = m.initializer;
-					let init = ini ? ini.getText(sourceFile) : "undefined";
-					let prop = getDecoratorArguments(m, "Prop", sourceFile);
-					let inject = getDecoratorArguments(m, "Inject", sourceFile);
-					if(prop) {
-						if(ini) props.push(`${name}:{type:${prop[0]},default:${init}}`);
-						else props.push(`${name}:${prop[0]}`);
-					} else if(inject !== undefined) {
-						if(!inject.length) inject[0] = `'${name}'`;
-						injects.push(`${name}: ${inject[0]}`);
-					} else {
-						data.push(`${name}:${init}`);
-						let provide = getDecoratorArguments(m, "Provide", sourceFile);
-						if(provide) provides.push(`${provide}: this.${name}`);
-					}
-				}
-				if(ts.isGetAccessor(m)) {
-					computed.push(getFunction(m, sourceFile));
-				}
-				if(ts.isMethodDeclaration(m)) {
-					let func = getFunction(m, sourceFile);
-					if(m.modifiers) for(let mod of m.modifiers) {
-						if(mod.kind == SyntaxKind.AsyncKeyword) {
-							func = "async " + func;
-							break;
-						}
-					}
-					let tag = getDecoratorArguments(m, "Watch", sourceFile);
-					if(tag) watch.push(getFunction(m, sourceFile, tag[0]));
-					else {
-						if(LIFECYCLE_HOOKS.includes(name)) events.push(func);
-						else methods.push(func);
-					}
-				}
-			}
-
-			let options: string[] = [`template: '${template}'`];
-
-			if(data.length) options.push(`data() { return { ${data.join(",")} }; }`);
-			if(props.length) options.push(`props: { ${props.join(",")} }`);
-			if(provides.length) options.push(`provide() { return { ${provides.join(",")} }; }`);
-			if(injects.length) options.push(`inject: { ${injects.join(",")} }`);
-			if(watch.length) options.push(`watch: { ${watch.join(",")} }`);
-			if(computed.length) options.push(`computed: { ${computed.join(",")} }`);
-			if(methods.length) options.push(`methods: { ${methods.join(",")} }`);
-			options.push(...events);
-
-			// Put into Vue component syntax
-			let output = `Vue.component('${comName}', { ${options.join(",")} });`;
-			// Transpile into JavaScript
-			output = ts.transpile(output, { target: ts.ScriptTarget.ESNext });
-			return { script: output, template: templateName };
-		}
+	if(template) {
+		options.unshift(`render() { ${compile(template).render} }`);
+	} else if('template' in option) {
+		options.unshift(`template: '#${comName}'`);
+	} else if(option.template) {
+		options.unshift(`template: '${option.template}'`);
+	} else {
+		throw new Error("Cannot resolve template");
 	}
+
+	// Put into Vue component syntax
+	let output = `Vue.component('${comName}', { ${options.join(",")} });`;
+
+	// Transpile into JavaScript
+	output = ts.transpile(output, { target: ts.ScriptTarget.ESNext });
+	return output;
 }
 
 export = VPDtoJs;
